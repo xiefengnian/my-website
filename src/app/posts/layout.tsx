b9b67b2e13f43fb81de45cfcff0789a0f27307a1
xiefengnian <xieniangao@126.com>
2024-03-19 14:51:49 +0800

    fix: react name

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, ReactNode } from "react";
 
-export default function({children}:{children:ReactNode}){
-    return <div style={{minHeight:'100vh'}}>{children}</div>
+export default function PostLayout({ children }: { children: ReactNode }) {
+  return <div style={{ minHeight: "100vh" }}>{children}</div>;
 }
