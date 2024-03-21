166a9b797298fd1be5c4591905fc946a2159dc6b
xiefengnian <xieniangao@126.com>
2024-03-21 11:33:37 +0800

    feat: remove token component

--- src/components/Token.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { theme } from "antd";
-import React, { PropsWithChildren } from "react";
-
-export default function Token({ children }: PropsWithChildren) {
-  const { token } = theme.useToken();
-  return React.cloneElement(children, { token, ...children.props });
-}
